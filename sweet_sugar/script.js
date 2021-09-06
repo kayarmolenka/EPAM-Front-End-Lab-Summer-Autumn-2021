6cff585f827633802bdfa74f875129b66818bb32
Kanstantsin <kayarmolenka@gmail.com>
2021-09-06 18:23:23 +0300

    fixed the remarks

--- sweet_sugar/script.js
+++ sweet_sugar/script.js
@@ -1,68 +1,58 @@
 class Base {
-    static #allUsers = []
-
     constructor(name) {
-      this.name = name;
-      Base.#allUsers.push(name);
+        this.name = name;
     }
-  
+
     sayHi() {
-      console.log(`Hello, I'm ${this.name}`);
+        console.log(`Hello, I'm ${this.name}`);
     }
 }
-  
+
 class Stuff extends Base {
-    constructor(name, stuff) {
+    constructor(name, ...stuff) {
         super(name);
         this.stuff = stuff;
     }
 }
 
-class Box extends Stuff {
-    showStuff() {
-        console.log(`I have ${this.stuff}`);
-    }
-}
-  
-class User extends Box {
-    gender;
-
-    constructor(name, stuff, surname) {
-        super(name, stuff);
-        this.surname = surname;
-    }
-
-    set gender(text) {
-        this.gender = text; 
-    }
+class Box extends Base {
+    stuff = [];
     
-    get gender() {
-        return `Your gender is ${this.gender}`
+    constructor(name, ...stuff1) {
+        super(name);
+        this.stuff.push(stuff1);
+        this.st = this.stuff; 		
+}
+
+    getStuffs() {
+        this.st.flat().map(el => console.log(el));
+    } 
+}
+
+const boxKosty = new Box('redBox', 'phone','pen', 'pencil');
+
+class User extends Base {
+    box = {stuff: boxKosty.stuff.flat()};
+
+    constructor(name, surname) {
+        super(name);
+        this.surname = surname;
+        this.littleBox = this.box;	
+    }
+
+    showStuffs() {
+        console.log(this.littleBox);
     }
 }
-  
-const data = {
-    kos: {
-        name: "Kanstantsin",
-        stuff: ["watch", "T-shirt", "phone"],
-        surname: "Yarmolenka"
-    }
-};
 
-const {kos: {name, stuff, surname }} = data;
-  
-const koly = new User("Koly", ["pen", "pencil"], 'Borisenko');
-const kosty = new User(name, stuff, surname);
+const stuff = new Stuff('stuffes of Kosty','apple','banana','watermelon');
+const kosty = new User('Kanstantsin','Yarmolenka');
 
-console.log('USER Kosty :', kosty);
-console.dir(Base);
 
-console.log(kosty.sayHi());
-console.log(kosty.showStuff());
+console.log('stuff :', stuff);
+console.log('boxKosty :', boxKosty);
+console.log('kosty :', kosty);
 
-console.log('USER Koly :', koly);
-console.log(koly.showStuff());
-
-kosty.gender = 'male';
-
-console.log('USER Kosty :', kosty)
+kosty.sayHi();
+kosty.showStuffs();
+boxKosty.getStuffs();
