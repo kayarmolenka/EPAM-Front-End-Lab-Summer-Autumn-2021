cd33866b78e3f2763c95e6f87e60caa96ac5ef92
Kanstantsin <kayarmolenka@gmail.com>
2021-10-12 00:16:37 +0300

    fixed remarks

--- network_interaction/script.js
+++ network_interaction/script.js
@@ -2,24 +2,24 @@ const $root = document.querySelector("#root");
 const $list = document.querySelector("#sel");
 const content = document.createElement("div");
 
+function createContent(array, value) {
+  array.forEach((el) => {
+    const wrapperForData = document.createElement("div");
+    wrapperForData.classList.add("item");
+    wrapperForData.style.backgroundColor = `rgb(${generatingNumbers()}, ${generatingNumbers()}, ${generatingNumbers()})`;
+    wrapperForData.textContent = el[value] || el;
+    content.append(wrapperForData);
+  });
+}
+
 $list.addEventListener("change", () => {
   $list.options[0].disabled = true;
   if ($list.value === "title") {
     fetch(`https://api.publicapis.org/entries`)
       .then((res) => res.json())
       .then((res) => {
-        async function createList() {
-          const data = await res;
-          content.innerHTML = "";
-          data.entries.forEach((el) => {
-            const div = document.createElement("div");
-            div.classList.add("item");
-            div.style.backgroundColor = `rgb(${generatingNumbers()}, ${generatingNumbers()}, ${generatingNumbers()})`;
-            div.textContent = el.API;
-            content.append(div);
-          });
-        }
-        createList();
+        content.innerHTML = "";
+        createContent(res.entries, "API");
       })
       .catch((err) => console.log("ERROOR!!!", err));
   }
@@ -28,13 +28,7 @@ $list.addEventListener("change", () => {
       .then((res) => res.json())
       .then((categories) => {
         content.innerHTML = "";
-        categories.forEach((category) => {
-          const div = document.createElement("div");
-          div.textContent = category;
-          div.classList.add("item");
-          div.style.backgroundColor = `rgb(${generatingNumbers()}, ${generatingNumbers()}, ${generatingNumbers()})`;
-          content.append(div);
-        });
+        createContent(categories);
       })
       .catch((err) => console.log("ERROOR!!!", err));
   }
